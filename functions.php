1c8939a50681acc36a95bcb5c7d708f553f6df1f
pianohagens <pianohagens@gmail.com>
2017-08-25 18:45:41 -0700

    created function and index php and upload to server

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Theme name: Web170_Temp
+Author: Piano Hagens
+Author URI: https://phagens.com/web170_temp/
+Description: Wordpress project start with sketch sheet
+Version: 1.0
+*/
+
+?
