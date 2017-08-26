54635b4c693fd786fc56894cb66b56a7022efb55
pianohagens <pianohagens@gmail.com>
2017-08-25 19:46:28 -0700

    start over, should start with home.html file to index.php
    Copy all home.html to index.php

--- index.php
+++ index.php
@@ -1,26 +1,58 @@
 <!doctype html>
 <html>
-<head>
-<meta charset="UTF-8">
+    <head>
+        <meta charset="utf-8">
+        <title>Web170_Temp</title>
+        
+<!----- Begin Styles --------------->
+<link rel="stylesheet" href="style.css" type="text/css" media="all" />
+<!----- End Styles --------------->
+        
+    </head><!----- End head --------------->
 
-<!-- Start Title Tag -->
-<title><?php get_my_title_tag(); ?></title>
-<!-- End Title Tag -->
-
-<!-- Begin Styles -->
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
     
-<!-- Start WP Head -->
-<?php wp_head(); ?>
-<!-- End WP Head -->
+    <body>
+    <!----- Begin Header --------------->
+    <div id="header">
+        
+    </div>
+    <!----- End Header ---------------> 
+    <!----- Begin Navigation --------------->
+<div id="navigation">
+     <ul>
+          <li class="home"><a href="#"><span>Home</span></a></li>
+          <li class="about"><a href="#"><span>About</span></a></li>
+          <li class="service"><a href="#"><span>Service</span></a></li>
+          <li class="products"><a href="#"><span>Products</span></a></li>
+          <li class="last contact"><a href="#"><span>Contact</span></a></li>
+     </ul>
+ </div>
+    <!----- End Navigation --------------->
+        
+    <!----- Begin Main --------------->
+    <div id="main">
+        <h2>This is my Main</h2>   
+    </div>
+    <!----- End Main --------------->
+        
+    <!----- Begin aside --------------->
+    <div id="aside">
+        <h3> This is my aside</h3>
+    </div>
+    <!----- End aside --------------->
+        
+        
+    <!----- Begin sidebar --------------->
+    <div id="sidebar">
+        <h3> This is my sidebar</h3>
+    </div>
+    <!----- End sidebar --------------->
+        
 
-</head>
-    
-<body <?php body_class();?>></body>
-
-
-<!-- Start WP Footer -->
-<?php wp_footer(); ?>
-<!-- End WP Footer -->
-    
+    </body>
+        <!----- End Footer --------------->
+     <div id="footer">
+<p>&copy;2017     |    All Right Reserved | Desigen be: Piano Hagens </p>   
+    </div>
+    <!----- End footer --------------->
 </html>
