2b6dd05c7e459a388519a1b8f4cbbebcf6ee5edd
pianohagens <pianohagens@gmail.com>
2017-08-25 19:28:06 -0700

    Add 4 functions to index.php
    1, bloginfo, in side the stylesheet link--><link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
    
    2, body_class, place it inside the body tag--> <body <?php body_class();?>></body>
    
    3, wp_head, --> placed it inside the end head tag area ---> <?php wp_head(); ?>
    4, wp_footer --> placed it after the body area --> <?php wp_footer(); ?>

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php //index.php?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+
+<!-- Start Title Tag -->
+<title><?php get_my_title_tag(); ?></title>
+<!-- End Title Tag -->
+
+<!-- Begin Styles -->
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
+    
+<!-- Start WP Head -->
+<?php wp_head(); ?>
+<!-- End WP Head -->
+
+</head>
+    
+<body <?php body_class();?>></body>
+
+
+<!-- Start WP Footer -->
+<?php wp_footer(); ?>
+<!-- End WP Footer -->
+    
+</html>
