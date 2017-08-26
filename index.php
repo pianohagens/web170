0b53a88ba8d100171afc49d06eb9ce7d54648e86
pianohagens <pianohagens@gmail.com>
2017-08-25 21:50:20 -0700

    Create The Loop within the content area: 1) The Title, 2) The Permalink and 3) The Content functions.
    1, <?php the_title( '<h1>', '</h1>' ); ?>
    
    2 and 3,
    <?php if (have_posts()) : ?>
            <?php while (have_posts()) : the_post(); ?>
    
                <article class="post-main" id="post-main-<?php the_ID(); ?>">
                    <h2><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h2>
                    <h4 class="postdate">Posted on <?php the_time('F j, Y'); ?> in <?php the_category(', ') ?> by <?php the_author(); ?></h4>
                    <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to"><?php the_post_thumbnail('thumbnail'); ?></a>
                    <?php the_excerpt(); ?>
                    <p class="readmore"><a href="<?php the_permalink(); ?>">Read More &raquo;</a> </p>
                </article>
            <?php endwhile; ?>
    <?php endif; ?>

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Web170_Temp</title>
+<head>
+<meta charset="utf-8">
+    
+<!-- Start Title Tag -->
+<?php the_title( '<h1>', '</h1>' ); ?>
+<!-- End Title Tag -->
         
 <!----- Begin Styles --------------->
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
@@ -35,7 +38,23 @@
         
     <!----- Begin Main --------------->
     <div id="main">
-        <h2>This is my Main</h2>   
+        <h2>This is my Main</h2>
+        
+    <!-- Begin Content -->    
+<?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+        
+            <article class="post-main" id="post-main-<?php the_ID(); ?>">
+                <h2><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h2>    
+                <h4 class="postdate">Posted on <?php the_time('F j, Y'); ?> in <?php the_category(', ') ?> by <?php the_author(); ?></h4>
+                <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to"><?php the_post_thumbnail('thumbnail'); ?></a>
+                <?php the_excerpt(); ?>
+                <p class="readmore"><a href="<?php the_permalink(); ?>">Read More &raquo;</a> </p>
+            </article>
+        <?php endwhile; ?>
+<?php endif; ?>
+    <!-- End Content -->
+        
     </div>
     <!----- End Main --------------->
         
